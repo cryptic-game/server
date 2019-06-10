12d846d38a615c61e86334d40975b0f0eafe6755
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2019-06-10 12:37:54 +0200

    Updated User.java to fix regex password problem
    Fixed regex. Old regex did not accept non letters in the first 8 chars.

--- src/main/java/net/cryptic_game/server/user/User.java
+++ src/main/java/net/cryptic_game/server/user/User.java
@@ -170,7 +170,7 @@ public class User {
     }
 
     public static boolean isValidPassword(String password) {
-        return Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}").matcher(password).find();
+        return Pattern.compile("(?=.*\\d)((?=.*[a-z])|(?=.*[A-Z]))(?=.*[!\"#$%`&\\\\'()*+,.\\/:;<=>?@\\[\\]^_{|}~-]).{8,}").matcher(password).find();
     }
 
     private static String hashPassword(String toHash) {
