f3788b52bbbc51e38c83948029b00a27f4029e37
Marcel Koch <34819524+MarcelCoding@users.noreply.github.com>
2020-08-23 00:17:40 +0200

    Improved initial connection attempts

--- src/main/java/net/cryptic_game/server/sql/SqlProperties.java
+++ src/main/java/net/cryptic_game/server/sql/SqlProperties.java
@@ -15,6 +15,8 @@ class SqlProperties extends Properties {
         this.put(Environment.CONNECTION_PREFIX + ".CharSet", charset.toString());
         this.put(Environment.CONNECTION_PREFIX + ".characterEncoding", charset.toString());
         this.put(Environment.CONNECTION_PREFIX + ".useUnicode", "true");
+        this.put("hibernate.hikari.connectionTimeout", "10000"); // 10 seconds
+        this.put("hibernate.hikari.initializationFailTimeout", "30000"); // 30 seconds
     }
 
     void setServer(final SqlServerType server, final String location, final String database) {
